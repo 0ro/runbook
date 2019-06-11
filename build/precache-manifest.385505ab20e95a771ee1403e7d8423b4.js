0d1249dedbdfbb375c3d4796abff258a9570f9ec
0ro <den.bogdanov4@gmail.com>
2019-06-11 15:19:34 +0300

    Add build files

---
+++ build/precache-manifest.385505ab20e95a771ee1403e7d8423b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d79342150f8b6fb4248fdd067f5cb81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8befeaa40c9f31e53d3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e8befeaa40c9f31e53d3",
+    "url": "/static/js/main.453d1035.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
