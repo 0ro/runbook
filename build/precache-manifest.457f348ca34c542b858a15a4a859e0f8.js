c6ab86b9898768454aaccd18a2604c274e846c13
0ro <den.bogdanov4@gmail.com>
2019-06-28 15:31:01 +0300

    Deploy

---
+++ build/precache-manifest.457f348ca34c542b858a15a4a859e0f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8423e85719f7776ff96c6114577fda",
+    "url": "https://0ro.github.io/runbook/build/index.html"
+  },
+  {
+    "revision": "2f04c22ac8216b6ff843",
+    "url": "https://0ro.github.io/runbook/build/static/css/main.2d9ec8a0.chunk.css"
+  },
+  {
+    "revision": "67b3da6bc45785126097",
+    "url": "https://0ro.github.io/runbook/build/static/js/2.37c666f8.chunk.js"
+  },
+  {
+    "revision": "2f04c22ac8216b6ff843",
+    "url": "https://0ro.github.io/runbook/build/static/js/main.6d1b66d6.chunk.js"
+  },
+  {
+    "revision": "a13e061f3b82296959a7",
+    "url": "https://0ro.github.io/runbook/build/static/js/runtime~main.63022cf7.js"
+  }
+]);
