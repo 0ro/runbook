26daf20b9cea7dd26d5852fd4a9e6651900d7bcb
0ro <den.bogdanov4@gmail.com>
2019-07-18 18:18:59 +0300

    Deploy

---
+++ build/precache-manifest.7a6a0cd172359d05e110e132c90ab875.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1448015198223cc0abd7425b6947e9",
+    "url": "https://0ro.github.io/runbook/build/index.html"
+  },
+  {
+    "revision": "0d94415b713a53986221",
+    "url": "https://0ro.github.io/runbook/build/static/css/main.2d9ec8a0.chunk.css"
+  },
+  {
+    "revision": "b4acb1f62de12c2719b4",
+    "url": "https://0ro.github.io/runbook/build/static/js/2.c2b91dc4.chunk.js"
+  },
+  {
+    "revision": "0d94415b713a53986221",
+    "url": "https://0ro.github.io/runbook/build/static/js/main.616265bd.chunk.js"
+  },
+  {
+    "revision": "a13e061f3b82296959a7",
+    "url": "https://0ro.github.io/runbook/build/static/js/runtime~main.63022cf7.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://0ro.github.io/runbook/build/precache-manifest.538336178a6bbeba80e4abb375e7f4e2.js"
+  "https://0ro.github.io/runbook/build/precache-manifest.7a6a0cd172359d05e110e132c90ab875.js"
 );
 
 self.addEventListener('message', (event) => {
